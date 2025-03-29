76f3f0b4858b917f4d52f33bfea1e02fe4aef230
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2025-03-29 14:44:48 +0700

    Update Mainwindow.cpp

--- Mainwindow.cpp
+++ Mainwindow.cpp
@@ -1,167 +1,220 @@
 #include "mainwindow.h"
 #include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
+    homePage(nullptr), registerPage(nullptr), loginPage(nullptr),
+    adminHome(nullptr), adminMember(nullptr), adminClub(nullptr), adminNoti(nullptr)
 {
-    // Create all pages
-    homePage = new HomePage();
-    registerPage = new RegisterPage();
-    loginPage = new LoginPage();
-    adminHome = new AdminHome();
-    adminMember = new AdminMember();
-    adminClub = new AdminClub();
-    adminNoti = new AdminNoti();
-
-    qDebug() << "All windows initialized";
-
-    // Set up all connections
-    setupConnections();
-
-    // Set initial window properties and show the home page
+    // Set initial window properties
     this->setWindowTitle("Club Management System");
     this->resize(375, 800);
 
-    // Show the initial page
-    showHomePage();
+    // Initialize only the home page on startup
+    initializeHomePage();
+    homePage->show();
 }
 
 MainWindow::~MainWindow()
 {
-    // Clean up all pages
-    delete homePage;
-    delete registerPage;
-    delete loginPage;
-    delete adminHome;
-    delete adminMember;
-    delete adminClub;
-    delete adminNoti;
+    // Clean up any pages that might still exist
+    if (homePage) delete homePage;
+    if (registerPage) delete registerPage;
+    if (loginPage) delete loginPage;
+    if (adminHome) delete adminHome;
+    if (adminMember) delete adminMember;
+    if (adminClub) delete adminClub;
+    if (adminNoti) delete adminNoti;
 }
 
-void MainWindow::setupConnections()
+void MainWindow::initializeHomePage()
 {
-    // HomePage connections
-    connect(homePage, &HomePage::openRegisterPage, this, &MainWindow::showRegisterPage);
-    connect(homePage, &HomePage::openLoginPage, this, &MainWindow::showLoginPage);
-
-    // RegisterPage connections
-    connect(registerPage, &RegisterPage::navigateToHome, this, &MainWindow::showHomePage);
-    connect(registerPage, &RegisterPage::openadminHome, this, &MainWindow::showAdminHomePage);
-
-    // LoginPage connections
-    connect(loginPage, &LoginPage::navigateToHome, this, &MainWindow::showHomePage);
-    connect(loginPage, &LoginPage::openadminHome, this, &MainWindow::showAdminHomePage);
-
-
-    // AdminHome connections
-    connect(adminHome, &AdminHome::navigateToMembers, this, &MainWindow::showAdminMemberPage);
-    connect(adminHome, &AdminHome::navigateToClubs, this, &MainWindow::showAdminClubPage);
-    connect(adminHome, &AdminHome::navigateToNotifications, this, &MainWindow::showAdminNotiPage);
-
-    // AdminMember connections
-    connect(adminMember, &AdminMember::navigateToHome, this, &MainWindow::showAdminHomePage);
-    connect(adminMember, &AdminMember::navigateToClubs, this, &MainWindow::showAdminClubPage);
-    connect(adminMember, &AdminMember::navigateToNotifications, this, &MainWindow::showAdminNotiPage);
-
-    // AdminClub connections
-    connect(adminClub, &AdminClub::navigateToHome, this, &MainWindow::showAdminHomePage);
-    connect(adminClub, &AdminClub::navigateToMembers, this, &MainWindow::showAdminMemberPage);
-    connect(adminClub, &AdminClub::navigateToNotifications, this, &MainWindow::showAdminNotiPage);
-
-    // AdminNoti connections
-    connect(adminNoti, &AdminNoti::navigateToHome, this, &MainWindow::showAdminHomePage);
-    connect(adminNoti, &AdminNoti::navigateToMembers, this, &MainWindow::showAdminMemberPage);
-    connect(adminNoti, &AdminNoti::navigateToClubs, this, &MainWindow::showAdminClubPage);
+    if (!homePage) {
+        homePage = new HomePage();
+        // Connect HomePage signals
+        connect(homePage, &HomePage::openRegisterPage, this, &MainWindow::showRegisterPage);
+        connect(homePage, &HomePage::openLoginPage, this, &MainWindow::showLoginPage);
+    }
 }
 
-// Navigation slot methods
+void MainWindow::initializeRegisterPage()
+{
+    if (!registerPage) {
+        registerPage = new RegisterPage();
+        // Connect RegisterPage signals
+        connect(registerPage, &RegisterPage::navigateToHome, this, &MainWindow::showHomePage);
+        connect(registerPage, &RegisterPage::openadminHome, this, &MainWindow::showAdminHomePage);
+    }
+}
+
+void MainWindow::initializeLoginPage()
+{
+    if (!loginPage) {
+        loginPage = new LoginPage();
+        // Connect LoginPage signals
+        connect(loginPage, &LoginPage::navigateToHome, this, &MainWindow::showHomePage);
+        connect(loginPage, &LoginPage::openadminHome, this, &MainWindow::showAdminHomePage);
+    }
+}
+
+void MainWindow::initializeAdminHomePage()
+{
+    if (!adminHome) {
+        adminHome = new AdminHome();
+        // Connect AdminHome signals
+        connect(adminHome, &AdminHome::navigateToMembers, this, &MainWindow::showAdminMemberPage);
+        connect(adminHome, &AdminHome::navigateToClubs, this, &MainWindow::showAdminClubPage);
+        connect(adminHome, &AdminHome::navigateToNotifications, this, &MainWindow::showAdminNotiPage);
+    }
+}
+
+void MainWindow::initializeAdminMemberPage()
+{
+    if (!adminMember) {
+        adminMember = new AdminMember();
+        // Connect AdminMember signals
+        connect(adminMember, &AdminMember::navigateToHome, this, &MainWindow::showAdminHomePage);
+        connect(adminMember, &AdminMember::navigateToClubs, this, &MainWindow::showAdminClubPage);
+        connect(adminMember, &AdminMember::navigateToNotifications, this, &MainWindow::showAdminNotiPage);
+    }
+}
+
+void MainWindow::initializeAdminClubPage()
+{
+    if (!adminClub) {
+        adminClub = new AdminClub();
+        // Connect AdminClub signals
+        connect(adminClub, &AdminClub::navigateToHome, this, &MainWindow::showAdminHomePage);
+        connect(adminClub, &AdminClub::navigateToMembers, this, &MainWindow::showAdminMemberPage);
+        connect(adminClub, &AdminClub::navigateToNotifications, this, &MainWindow::showAdminNotiPage);
+    }
+}
+
+void MainWindow::initializeAdminNotiPage()
+{
+    if (!adminNoti) {
+        adminNoti = new AdminNoti();
+        // Connect AdminNoti signals
+        connect(adminNoti, &AdminNoti::navigateToHome, this, &MainWindow::showAdminHomePage);
+        connect(adminNoti, &AdminNoti::navigateToMembers, this, &MainWindow::showAdminMemberPage);
+        connect(adminNoti, &AdminNoti::navigateToClubs, this, &MainWindow::showAdminClubPage);
+    }
+}
+
+// Function to destroy unused pages to free memory
+void MainWindow::cleanupUnusedPages(QWidget* currentPage)
+{
+    // Don't destroy the current page
+    if (homePage && homePage != currentPage) {
+        delete homePage;
+        homePage = nullptr;
+        qDebug() << "Home page destroyed";
+    }
+
+    if (registerPage && registerPage != currentPage) {
+        delete registerPage;
+        registerPage = nullptr;
+        qDebug() << "Register page destroyed";
+    }
+
+    if (loginPage && loginPage != currentPage) {
+        delete loginPage;
+        loginPage = nullptr;
+        qDebug() << "Login page destroyed";
+    }
+
+    if (adminHome && adminHome != currentPage) {
+        delete adminHome;
+        adminHome = nullptr;
+        qDebug() << "Admin home page destroyed";
+    }
+
+    if (adminMember && adminMember != currentPage) {
+        delete adminMember;
+        adminMember = nullptr;
+        qDebug() << "Admin member page destroyed";
+    }
+
+    if (adminClub && adminClub != currentPage) {
+        delete adminClub;
+        adminClub = nullptr;
+        qDebug() << "Admin club page destroyed";
+    }
+
+    if (adminNoti && adminNoti != currentPage) {
+        delete adminNoti;
+        adminNoti = nullptr;
+        qDebug() << "Admin notification page destroyed";
+    }
+}
+
+// Navigation methods
 void MainWindow::showHomePage()
 {
     qDebug() << "Showing Home Page";
+    initializeHomePage();
     homePage->show();
-    registerPage->hide();
-    loginPage->hide();
-    adminHome->hide();
-    adminMember->hide();
-    adminClub->hide();
-    adminNoti->hide();
+    // Clean up unused pages
+    cleanupUnusedPages(homePage);
 }
 
 void MainWindow::showRegisterPage()
 {
     qDebug() << "Showing Register Page";
-    homePage->hide();
+    initializeRegisterPage();
     registerPage->show();
-    loginPage->hide();
-    adminHome->hide();
-    adminMember->hide();
-    adminClub->hide();
-    adminNoti->hide();
+    // Clean up unused pages
+    cleanupUnusedPages(registerPage);
 }
 
 void MainWindow::showLoginPage()
 {
     qDebug() << "Showing Login Page";
-    homePage->hide();
-    registerPage->hide();
+    initializeLoginPage();
     loginPage->show();
-    adminHome->hide();
-    adminMember->hide();
-    adminClub->hide();
-    adminNoti->hide();
+    // Clean up unused pages
+    cleanupUnusedPages(loginPage);
 }
 
 void MainWindow::showAdminHomePage()
 {
     qDebug() << "Showing Admin Home Page";
-    homePage->hide();
-    registerPage->hide();
-    loginPage->hide();
+    initializeAdminHomePage();
     adminHome->show();
     adminHome->raise();
     adminHome->activateWindow();
-    adminMember->hide();
-    adminClub->hide();
-    adminNoti->hide();
+    // Clean up unused pages
+    cleanupUnusedPages(adminHome);
 }
 
 void MainWindow::showAdminMemberPage()
 {
     qDebug() << "Showing Admin Member Page";
-    homePage->hide();
-    registerPage->hide();
-    loginPage->hide();
-    adminHome->hide();
+    initializeAdminMemberPage();
     adminMember->show();
     adminMember->raise();
     adminMember->activateWindow();
-    adminClub->hide();
-    adminNoti->hide();
+    // Clean up unused pages
+    cleanupUnusedPages(adminMember);
 }
 
 void MainWindow::showAdminClubPage()
 {
     qDebug() << "Showing Admin Club Page";
-    homePage->hide();
-    registerPage->hide();
-    loginPage->hide();
-    adminHome->hide();
-    adminMember->hide();
+    initializeAdminClubPage();
     adminClub->show();
     adminClub->raise();
     adminClub->activateWindow();
-    adminNoti->hide();
+    // Clean up unused pages
+    cleanupUnusedPages(adminClub);
 }
 
 void MainWindow::showAdminNotiPage()
 {
     qDebug() << "Showing Admin Notification Page";
-    homePage->hide();
-    registerPage->hide();
-    loginPage->hide();
-    adminHome->hide();
-    adminMember->hide();
-    adminClub->hide();
+    initializeAdminNotiPage();
     adminNoti->show();
     adminNoti->raise();
     adminNoti->activateWindow();
+    // Clean up unused pages
+    cleanupUnusedPages(adminNoti);
 }
