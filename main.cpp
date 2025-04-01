5ab4bd98595870138d80655d43564953dc20d184
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2025-04-01 23:36:44 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,12 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    // Initialize the student names list
+    if (!Database::initializeStudentNames()) {
+        qDebug() << "Warning: Failed to initialize student names list!";
+        // Continue anyway as this is not critical
+    }
+
     // Create the main window instead of individual pages
     MainWindow mainWindow;
 
