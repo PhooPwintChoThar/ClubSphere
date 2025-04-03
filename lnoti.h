c9b278a7149bbb9d1a1f99a881a9d039a38c3f34
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2025-04-03 14:09:09 +0700

    Update lnoti.h

--- lnoti.h
+++ lnoti.h
@@ -8,13 +8,20 @@
 #include <QPushButton>
 #include <QVector>
 #include <QFrame>
+#include <QDateTime>
 
-class UserRequest {
+// Updated notification struct to match database schema
+class Notification {
 public:
-    QString name;
-    int points;
+    int clubId;
+    int userId;
+    QString content;
+    bool isRequest; // 1 = request, 0 = notification
+    qint64 timestamp;
+    QString username;
 
-    UserRequest(const QString &n, int p) : name(n), points(p) {}
+    Notification(int cId, int uId, const QString &cont, bool isReq, qint64 time, const QString &name)
+        : clubId(cId), userId(uId), content(cont), isRequest(isReq), timestamp(time), username(name) {}
 };
 
 class LNoti : public QWidget
@@ -22,9 +29,9 @@ class LNoti : public QWidget
     Q_OBJECT
 
 public:
-    explicit LNoti(QWidget *parent = nullptr);
+    explicit LNoti(int clubId, QWidget *parent=nullptr);
     ~LNoti();
-
+    void prepareForDestruction();
 signals:
     void navigateBack();
 
@@ -34,18 +41,26 @@ private slots:
     void onRejectClicked(int index);
 
 private:
+    QPixmap createCircularPixmap(const QPixmap &srcPixmap);
+    QPixmap getUserProfilePhoto(int userId);
     void setupUI();
-    void setupNotificationsArea(); // Add this function declaration
-    QWidget* createUserRequestWidget(const UserRequest &request, int index);
+    void setupNotificationsArea();
+    void loadNotificationsFromDatabase();
+    void updateUserClubStatus(int userId, int clubId, bool accepted);
+    void refreshNotifications();
+    QWidget* createNotificationWidget(const Notification &notification, int index);
     QWidget* createHeaderWidget();
+    QString formatTimestamp(qint64 timestamp);
 
     QVBoxLayout *m_mainLayout;
     QScrollArea *m_scrollArea;
     QWidget *m_scrollContent;
-    QVBoxLayout *m_requestsLayout;
+    QVBoxLayout *m_notificationsLayout;
 
-    QVector<UserRequest> m_requests;
-    QVector<QWidget*> m_requestWidgets;
+    QVector<Notification> m_notifications;
+    QVector<QWidget*> m_notificationWidgets;
+
+    int m_clubId; // The clubId passed to this page
 };
 
 #endif // LNOTI_H
