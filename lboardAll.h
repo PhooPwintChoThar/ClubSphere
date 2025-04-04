8c176897f4545179e2061d58c8a600008bd28ef1
Phoo Pwint Cho Thar <phoopwintchothar1@gmail.com>
2025-04-04 15:43:47 +0700

    Update lboardAll.h

--- lboardAll.h
+++ lboardAll.h
@@ -1,97 +1,58 @@
-#ifndef LBOARDALL_H
-#define LBOARDALL_H
+#ifndef LEADERBOARDALL_H
+#define LEADERBOARDALL_H
 
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
-#include <QPushButton>
 #include <QLabel>
-#include <QFrame>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QButtonGroup>
 #include <QScrollArea>
+#include <QSqlQuery>
+#include <QPixmap>
+#include <QPainter>
 
 class LeaderboardAll : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit LeaderboardAll(QWidget *parent = nullptr);
+    enum TabType { ClubMembers, Members, Clubs };
+
+    explicit LeaderboardAll(int club_id, QWidget *parent = nullptr);
     ~LeaderboardAll();
 
 signals:
     void navigateBack();
 
 private slots:
-    void onBackButtonClicked();
-    void onAllTabClicked();
+    void onClubMembersTabClicked();
     void onMembersTabClicked();
     void onClubsTabClicked();
 
 private:
     void setupUI();
-    void setupTabBar();
-    void setupTopRankersSection();
-    void setupRankingListSection();
-    void updateTabButtonStyles();
-    QLabel* createMedalLabel(const QString &color);
-    QWidget* createTopRankerWidget(const QString &medalType, const QString &name, int score);
-    QWidget* createRankListItemWidget(const QString &name, int score);
-    QWidget* createTopUserWidget(const QString &medalType, const QString &name);
-    QWidget* createTopClubWidget(const QString &medalType, const QString &name);
+    void setupTopLeaders();
+    void setupListItems();
+    void clearLeaderboard();
+    QWidget* createLeaderItem(const QString& name, int idOrPoints, bool isTopThree, int rank);
+    void showClubMembersLeaderboard();
+    void showMembersLeaderboard();
+    void showClubsLeaderboard();
 
-    // Member data
-    struct MemberData {
-        QString name;
-        int score;
-    };
+    int m_clubId;
+    TabType m_currentTab;
 
-    // Club data
-    struct ClubData {
-        QString name;
-        int score;
-    };
-
-    void loadMemberData();
-    void loadClubData();
-    void updateMembersView();
-    void updateClubsView();
-    void updateAllView();
-    void createSectionHeader(QVBoxLayout *layout, const QString &title);
-
-    QList<MemberData> memberList;
-    QList<ClubData> clubList;
-
-    // State tracking
-    enum TabState {
-        ALL_TAB,
-        MEMBERS_TAB,
-        CLUBS_TAB
-    };
-    TabState currentTab;
-
-    // Main layout and components
-    QVBoxLayout *mainLayout;
-
-    // Header components
-    QWidget *headerWidget;
-    QHBoxLayout *headerLayout;
-    QPushButton *backButton;
-    QLabel *titleLabel;
-
-    // Tab components
-    QWidget *tabWidget;
-    QHBoxLayout *tabLayout;
-    QPushButton *allTabButton;
-    QPushButton *membersTabButton;
-    QPushButton *clubsTabButton;
-
-    // Top Rankers section
-    QWidget *topRankersWidget;
-    QHBoxLayout *topRankersLayout;
-
-    // Rankings list section (scrollable)
-    QScrollArea *rankingsScrollArea;
-    QWidget *rankingsListWidget;
-    QVBoxLayout *rankingsListLayout;
+    QVBoxLayout* m_mainLayout;
+    QLabel* m_titleLabel;
+    QButtonGroup* m_tabGroup;
+    QRadioButton* m_clubMembersTab;
+    QRadioButton* m_membersTab;
+    QRadioButton* m_clubsTab;
+    QWidget* m_topThreeWidget;
+    QScrollArea* m_scrollArea;
+    QVBoxLayout* m_leaderListLayout;
 };
 
-#endif // LBOARDALL_H
+#endif // LEADERBOARDALL_H
